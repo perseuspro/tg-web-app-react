888561bf5207a1b33ba6ae3c7ad3ae587de4abb9
perseuspro <igor.dyukar@gmail.com>
2023-05-05 17:10:05 +0300

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { useEffect } from 'react';
 import './App.css';
+import { useTelegram } from "../../hooks/useTelegram";
+
 
 
 function App() {
+  const {onTogleButton, tg} = useTelegram()
 
+  useEffect(()=>{
+    tg.ready()
+  },[])
+
+  
   return (
     <div className="App">
-      
+      <button>toggle</button>
     </div>
   );
 }
---
+++ src/hooks/useTelegram.js
@@ -0,0 +1,24 @@
+const tg = window.Telegram.WebApp;
+
+export function useTelegram(){
+	
+
+
+	const onClose =()=>{
+		tg.close()
+	}
+
+	const onTogleButton =()=>{
+		if(tg.MainButton.isVisible) {
+			tg.MainButton.hide()
+		} else {
+			tg.MainButton.show()
+		}
+	}
+	return {
+		onClose,
+		onTogleButton,
+		tg,
+		user: tg.initDataUnsafe?.user?.username
+	}
+}
